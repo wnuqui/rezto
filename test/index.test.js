55c8b96c52fbdd571fc789632c621fc55cd76e16
Wilfrido Nuqui <dofreewill22@gmail.com>
2017-06-12 18:12:43 +0800

    Add unit tests for `getRestaurants`

--- test/index.test.js
+++ test/index.test.js
@@ -1,13 +1,51 @@
 import { expect } from 'chai'
-import rezto from '../src/index'
-import { TRIPADVISOR_URL } from '../src/constants'
+import _          from 'lodash'
+import rezto      from '../src/index'
+import nock       from 'nock'
 
-describe('rezto', () => {
-  it('defines getRestaurants', () => {
-    expect(typeof rezto.getRestaurants).to.equal('function')
+describe('getRestaurants', () => {
+  context('success', () => {
+    beforeEach(() => {
+      nock('https://www.tripadvisor.com.ph')
+        .get(/RestaurantSearch/)
+        .replyWithFile(200, __dirname + '/fixtures/restaurants.html') /* eslint no-path-concat: 0 */
+
+      nock('https://www.tripadvisor.com.ph')
+        .get(/Restaurant_Review/)
+        .times(30)
+        .replyWithFile(200, __dirname + '/fixtures/restaurant-detail.html')
+    })
+
+    it('async return restaurants', async () => {
+      const options = {'parent': {'place': 'mandaluyong', 'type': 'restaurants', 'sortBy': 'rank', 'batchNumber': 1}}
+      let restaurants
+
+      await rezto.getRestaurants(options).then((resolvedValue) => {
+        restaurants = resolvedValue
+      })
+
+      _.forIn(restaurants, (value, key) => {
+        expect(restaurants[key]).to.eq(value)
+      })
+    })
   })
 
-  it('defines TRIPADVISOR_URL', () => {
-    expect(typeof TRIPADVISOR_URL).to.equal('string')
+  context('error', () => {
+    beforeEach(() => {
+      nock('https://www.tripadvisor.com.ph')
+        .get(/RestaurantSearch/)
+        .replyWithError('error')
+    })
+
+    it('rejects with an error', async () => {
+      const options = {'parent': {}}
+      let error = false
+
+      await rezto.getRestaurants(options).catch(() => {
+        error = true
+      })
+
+      expect(error).to.eq(true)
+    })
   })
 })
